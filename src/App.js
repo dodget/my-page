853e6a18748e05c9ba9626c2879c4d1ee6ca8205
Tylor Dodge <tylor.dodge@aashe.org>
2019-01-21 12:44:51 -0500

    Final update before release

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Nav, NavItem, Container, Row, Col } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUser,
+  faCode,
+  faUniversalAccess,
+  faLeaf,
+  faAt
+} from "@fortawesome/free-solid-svg-icons";
+
+import Me from "./components/Me";
+import Access from "./components/Access";
+import Ethos from "./components/Ethos";
+import Contact from "./components/Contact";
+import Work from "./components/Work";
+import BackgroundVideo from "./components/BackgroundVideo";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      content: "",
+      activeTab: "1"
+    };
+  }
+
+  handleToggleContent = content => {
+    if (this.state.content !== content) {
+      this.setState({ content });
+    } else {
+      this.setState({ content: "" });
+    }
+  };
+
+  handleToggleWorkTab = tab => {
+    if (this.state.activeTab !== tab) {
+      this.setState({ activeTab: tab });
+    }
+  };
+
+  getClass = type => {
+    if (type === this.state.content) {
+      return "clicked";
+    }
+    return "icon";
+  };
+
   render() {
+    const types = [
+      ["me", faUser, "About me."],
+      ["work", faCode, "My work."],
+      ["access", faUniversalAccess, "Web accessibility."],
+      ["ethos", faLeaf, "My ethos."],
+      ["contact", faAt, "Contact me."]
+    ];
+
+    let cont =
+      this.state.content === "access" ? (
+        <Access />
+      ) : this.state.content === "me" ? (
+        <Me />
+      ) : this.state.content === "ethos" ? (
+        <Ethos />
+      ) : this.state.content === "work" ? (
+        <Work
+          handleToggleWorkTab={this.handleToggleWorkTab}
+          activeTab={this.state.activeTab}
+        />
+      ) : this.state.content === "contact" ? (
+        <Contact />
+      ) : (
+        <BackgroundVideo />
+      );
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Row>
+          <Col xs="3" sm="2" md="1">
+            <Nav vertical fill className="side-nav">
+              {types.map(t => {
+                let icon = t[0] === this.state.content ? "clicked" : "icon";
+                let divType =
+                  t[0] === this.state.content ? "nav-div-clicked" : "nav-div";
+                return (
+                  <NavItem key={t[0]}>
+                    <div
+                      className={divType}
+                      onClick={() => this.handleToggleContent(t[0])}
+                      tabIndex="0"
+                    >
+                      <FontAwesomeIcon
+                        icon={t[1]}
+                        className={icon}
+                        title={t[2]}
+                      />
+                    </div>
+                  </NavItem>
+                );
+              })}
+            </Nav>
+          </Col>
+          <Col>
+            <Container>
+              <Col
+                sm="11"
+                md={{ size: 9, offset: 1 }}
+                className="content-block"
+              >
+                {cont}
+              </Col>
+            </Container>
+          </Col>
+        </Row>
       </div>
     );
   }
