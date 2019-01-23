eb5aae77043c1d7167c263f00770acb0046fc165
Tylor Dodge <tylor.dodge@aashe.org>
2019-01-23 07:22:49 -0500

    Updates

---
+++ precache-manifest.ea5cd97d8c12b29ab4133e54e0cb7b21.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "23f1751222b3a6547a29add07e1bed59",
+    "url": "/static/media/winter_trim.23f17512.mp4"
+  },
+  {
+    "revision": "2dc5d432d76332b3fba16a9c1ea9ac01",
+    "url": "/static/media/fall_tree.2dc5d432.mp4"
+  },
+  {
+    "revision": "238af632bdc75d52c733aa828af55336",
+    "url": "/static/media/Purple_And_White_Flowers.238af632.mp4"
+  },
+  {
+    "revision": "9d6aca94826e01b12c1a5cbf975f75cc",
+    "url": "/static/media/Grass_and_Flowers.9d6aca94.mp4"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f370cee1cb6b01e4122e",
+    "url": "/static/js/main.f370cee1.chunk.js"
+  },
+  {
+    "revision": "f9024d87d21c39b8b664",
+    "url": "/static/js/1.f9024d87.chunk.js"
+  },
+  {
+    "revision": "f370cee1cb6b01e4122e",
+    "url": "/static/css/main.0102594f.chunk.css"
+  },
+  {
+    "revision": "f9024d87d21c39b8b664",
+    "url": "/static/css/1.018b2cb1.chunk.css"
+  },
+  {
+    "revision": "beab666e246e4743ff316fc65e17cd37",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-page/precache-manifest.e4ffa75db756efeacb81c28f13dc9be5.js"
+  "/precache-manifest.ea5cd97d8c12b29ab4133e54e0cb7b21.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/my-page/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
